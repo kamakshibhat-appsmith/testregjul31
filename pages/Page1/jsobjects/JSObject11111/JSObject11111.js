2f5fa66d08abe87cf5bc1d12e575fb2179b32b44
kamakshi+1 <kamakshi+1@appsmith.com>
2024-07-31 13:31:09 +0000

    aaa

---
+++ pages/Page1/jsobjects/JSObject11111/JSObject11111.js
@@ -0,0 +1,12 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1aq () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
---
+++ pages/Page1/jsobjects/wewewe/wewewe.js
@@ -0,0 +1,13 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun11q () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+		return momentFun1.myFun2()
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
